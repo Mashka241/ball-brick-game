98027de82755ebb7ad202ddc1f05ce88f7a4bdb5
Maria Ivanova <ivanova.m241@gmail.com>
2020-02-20 19:10:13 +0300

    add bricks

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,33 @@ window.onload = () => {
   const canvas = document.getElementById('gameCanvas')
   const ctx = canvas.getContext('2d')
   const framesPerSecond = 30
+
   const PADDLE_WIDTH = 100
   const PADDLE_THICKNESS = 10
   const PADDLE_DIST_FROM_EDGE = 60
+  const BRICK_WIDTH = 100
+  const BRICK_HEIGHT = 50
+  const BRICK_COUNT = 8
+
+  const brickGrid = new Array(BRICK_COUNT).fill(false)
+
+  const resetBricks = () => {
+    for (var i = 0; i < brickGrid.length; i++) {
+      brickGrid[i] = true
+    }
+
+    // brickGrid.map(el => !el && true)
+    console.log('reset', brickGrid)
+  }
 
   let ballX = 75
   let ballSpeedX = 5
   let ballY = 75
   let ballSpeedY = 7
   let paddleX = 400
+
+  let mouseX = 0
+  let mouseY = 0
 
   const moveAll = () => {
     ballX += ballSpeedX
@@ -39,7 +57,6 @@ window.onload = () => {
 
       const centerOfPaddleX = paddleX + PADDLE_WIDTH / 2
       const ballDistFromPaddleCenterX = ballX - centerOfPaddleX
-      console.log(ballDistFromPaddleCenterX)
       ballSpeedX = ballDistFromPaddleCenterX * 0.35
     }
   }
@@ -56,10 +73,27 @@ window.onload = () => {
     ctx.fill()
   }
 
+  const colorText = (text, textX, textY, fillColor) => {
+    ctx.fillStyle = fillColor
+    ctx.fillText(text, textX, textY)
+  }
+
+  const drawBricks = () => {
+    console.log('draw bricks', brickGrid)
+    for (let i = 0; i < brickGrid.length; i++) {
+      if (brickGrid[i]) {
+        const x = i * (BRICK_WIDTH)
+        colorRect(x, 0, BRICK_WIDTH - 2, BRICK_HEIGHT, 'blue')
+      }
+    }
+  }
+
   const drawAll = () => {
     colorRect(0, 0, canvas.width, canvas.height, 'black') // clear screen
     colorCircle(ballX, ballY, 10, 'white') // draw ball
     colorRect(paddleX, canvas.height - PADDLE_DIST_FROM_EDGE, PADDLE_WIDTH, PADDLE_THICKNESS, 'white') // draw paddle
+    drawBricks()
+    colorText(`${mouseX}, ${mouseY}`, mouseX, mouseY, 'white')
   }
 
   const updateAll = () => {
@@ -71,8 +105,10 @@ window.onload = () => {
     const rect = canvas.getBoundingClientRect()
     // const root = document.documentElement
     // const mouseX = evt.clientX - rect.left - root.scrollLeft
-    const mouseX = evt.clientX - rect.left
     // const mouseY = evt.clientY - rect.top - root.scrollTop
+    mouseX = evt.clientX - rect.left
+    mouseY = evt.clientY - rect.top
+
     paddleX = mouseX - PADDLE_WIDTH / 2
   }
 
@@ -84,4 +120,5 @@ window.onload = () => {
   setInterval(updateAll, 1000 / framesPerSecond)
 
   canvas.addEventListener('mousemove', updateMousePos)
+  resetBricks()
 }
